3566f1b5081af284786fb4b80b6db39bd0e2dfcf
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-15 12:21:25 +0200

    Fix typo in max.test.js documentation for clarity

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -1,9 +1,9 @@
 /* Find the maximum element of an array of numbers
 
-In this kata, you will need to implement a function that find the largest numerical element of an array.
+In this kata, you will need to implement a function that finds the largest numerical element of an array.
 
 E.g. max([30, 50, 10, 40]), target output: 50
-E.g. max(['hey', 10, 'hi', 60, 10]), target output: 60 (sum ignores any non-numerical elements)
+E.g. max(['hey', 10, 'hi', 60, 10]), target output: 60 (findMax ignores any non-numerical elements)
 
 You should implement this function in max.js, and add tests for it in this file.
 
