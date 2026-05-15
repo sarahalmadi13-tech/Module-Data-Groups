e0bab69d32a1becc18babdc508f90d6432d002e8
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-15 12:53:35 +0200

    Implement contains function to check for property existence in an object

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(object, propertyName) {
+  if (typeof object !== "object" || object === null) return false;
+  if (Array.isArray(object)) {
+    throw new Error("Invalid parameter");
+  }
+  return propertyName in object;
+}
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -1,35 +1,29 @@
-const contains = require("./contains.js");
-
-/*
-Implement a function called contains that checks an object contains a
-particular property
-
-E.g. contains({a: 1, b: 2}, 'a') // returns true
-as the object contains a key of 'a'
-
-E.g. contains({a: 1, b: 2}, 'c') // returns false
-as the object doesn't contains a key of 'c'
-*/
-
-// Acceptance criteria:
-
-// Given a contains function
-// When passed an object and a property name
-// Then it should return true if the object contains the property, false otherwise
+const contains = require("./contains");
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+  expect(contains({}, "a")).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("Given an object with existing property name, return true", () => {
+  expect(contains({ a: 1, b: 2 }, "a")).toBe(true);
+});
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("Given an object with a non-existent property name, return false", () => {
+  expect(contains({ a: 1, b: 2 }, "c")).toBe(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
-// Then it should return false or throw an error
+// Then it should throw an error
+test("Given invalid parameter like array, throw error", () => {
+  expect(() => contains([1, 2, 5], "a")).toThrow("Invalid parameter");
+});
