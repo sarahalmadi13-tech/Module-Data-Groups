d4e57d52282e451a5c3825bd73c94524f32f2e68
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-15 12:19:24 +0200

    Add findMax function to determine the maximum value in an array

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,21 @@
 function findMax(elements) {
+  if (!Array.isArray(elements)) return -Infinity;
+  let max = -Infinity;
+  for (let i = 0; i < elements.length; i++) {
+    if (
+      typeof elements[i] === "number" &&
+      !isNaN(elements[i]) &&
+      elements[i] > max
+    ) {
+      max = elements[i];
+    }
+  }
+  return max;
 }
+console.log(findMax([-12, 15, 3, -6, -1])); // should return 15, because it's the largest number overall
+console.log(findMax([-12, -15, -3, -6, -1])); // should return -1, because it's the closest one to zero
+console.log(findMax([2.5, 0.3, 0.8, 5.1, 3.9])); // should return 5.1, because it's the largest decimal number
+console.log(findMax([2.5, "potato", 0.3, null, 0.8, undefined, 5.1, 3.9])); // should return 5.1, because it's the largest number and non-numeric values are ignored
+console.log(findMax(["kiwis", "potato", undefined])); // should return -Infinity, because there are no numeric values and non-numeric values are ignored
 
 module.exports = findMax;
