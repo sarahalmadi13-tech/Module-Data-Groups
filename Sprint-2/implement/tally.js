f166fa20f3015815f9dafc23d70fe218b254dbc5
Sarah Almadi <sarah666lilith@gmail.com>
2026-05-15 13:06:32 +0200

    Implement tally function to count item frequencies in an array and add corresponding tests

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,18 @@
-function tally() {}
+function tally(input) {
+  if (!Array.isArray(input)) {
+    throw new Error("not an Array");
+  }
+  let counter = {};
+
+  for (let item of input) {
+    if (counter[item] === undefined) {
+      counter[item] = 1;
+    } else {
+      counter[item] += 1;
+    }
+  }
+
+  return counter;
+}
 
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -1,34 +1,23 @@
-const tally = require("./tally.js");
-
-/**
- * tally array
- *
- * In this task, you'll need to implement a function called tally
- * that will take a list of items and count the frequency of each item
- * in an array
- *
- * For example:
- *
- * tally(['a']), target output: { a: 1 }
- * tally(['a', 'a', 'a']), target output: { a: 3 }
- * tally(['a', 'a', 'b', 'c']), target output: { a : 2, b: 1, c: 1 }
- */
-
-// Acceptance criteria:
-
-// Given a function called tally
-// When passed an array of items
-// Then it should return an object containing the count for each unique item
-
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
 test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+  expect(tally([])).toEqual({});
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
 
+test("Given an array with duplicated items, return counts for each unique item", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+});
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("Given an invalid input such as a string, should throw an error: not an Array", () => {
+  expect(() => {
+    tally("hello");
+  }).toThrow("not an Array");
+});
